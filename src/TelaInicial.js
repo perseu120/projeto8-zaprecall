230777ab2ea85ebf1a44c592a468a01a9c65e404
josimar <josimar.17perseu@gmail.com>
2022-05-12 20:53:55 -0300

    testando o versel

---
+++ src/TelaInicial.js
@@ -0,0 +1,11 @@
+import imagem from "/home/perseu/Documentos/Driven/projeto8-zaprecall/src/assets/imagem/logo.png"
+export default function TelaInicial(){
+
+    return (
+        <div>
+            <img src={imagem} alt=""/>
+           
+        </div>
+    )
+
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import TelaInicial from './TelaInicial';
+
+function App(){
+
+    return(
+        <>
+           <TelaInicial />
+        </>
+        
+    )
+
+}
+
+ReactDOM.render(App(), document.querySelector('.root'));
